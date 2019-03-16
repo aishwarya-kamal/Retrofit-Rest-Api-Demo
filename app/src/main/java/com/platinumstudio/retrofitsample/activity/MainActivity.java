59fe1e71c1d1472601c544c3266fda793a67060d
aishwaryakamal <aishwaryakamal5@gmail.com>
2019-03-16 20:23:38 +0800

    Added required dependencies, Recyclerview in activity_main.xml and Internet permission

---
+++ app/src/main/java/com/platinumstudio/retrofitsample/activity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.platinumstudio.retrofitsample.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.platinumstudio.retrofitsample.R;
+//import com.platinumstudio.retrofitsample.adapter.CustomAdapter;
+//import com.platinumstudio.retrofitsample.model.Photo;
+//import com.platinumstudio.retrofitsample.network.GetDataService;
+//import com.platinumstudio.retrofitsample.network.RetrofitClientInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
